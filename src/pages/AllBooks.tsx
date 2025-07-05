9785a1f339e8c2e6662b2cc8b625f5ee2979d66a
Shahariar Sohan <Sohanshahariar4@gmail.com>
2025-07-05 10:51:35 +0600

    modification done in book summary page

--- src/pages/AllBooks.tsx
+++ src/pages/AllBooks.tsx
@@ -27,7 +27,6 @@ const AllBooks = () => {
       )}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 p-5 my-5">
         {!isLoading &&
-          !isError &&
           books?.map((book: IBook, idx: number) => (
             <BookCard key={idx} {...book}></BookCard>
           ))}
--- src/pages/BorrowSummary.tsx
+++ src/pages/BorrowSummary.tsx
@@ -26,26 +26,32 @@ const BorrowSummary = () => {
   return (
     <div className="container mx-auto">
       <h1 className="text-2xl font-bold text-center">Borrowed Book Summary</h1>
-      {}
+
       <div className="overflow-x-auto mt-10 p-4">
         <div className="min-w-full bg-white rounded-md shadow-md overflow-hidden mb-10">
           <table className="min-w-full divide-y divide-gray-200">
-            <thead className="bg-gray-800">
-              <tr>
-                <th className="px-6 py-4 text-left text-sm font-medium text-white tracking-wider">
-                  Book Title
-                </th>
-                <th className="px-6 py-4 text-left text-sm font-medium text-white tracking-wider">
-                  ISBN
-                </th>
-                <th className="px-6 py-4 text-left text-sm font-medium text-white tracking-wider">
-                  Total Quantity
-                </th>
-              </tr>
-            </thead>
+            {!isLoading && borrowedBooks.length > 0 ? (
+              <thead className="bg-gray-800">
+                <tr>
+                  <th className="px-6 py-4 text-left text-sm font-medium text-white tracking-wider">
+                    Book Title
+                  </th>
+                  <th className="px-6 py-4 text-left text-sm font-medium text-white tracking-wider">
+                    ISBN
+                  </th>
+                  <th className="px-6 py-4 text-left text-sm font-medium text-white tracking-wider">
+                    Total Quantity
+                  </th>
+                </tr>
+              </thead>
+            ) : (
+              <h1 className="text-xl font-bold text-red-500 text-center my-5">
+                No Book Summary Available....
+              </h1>
+            )}
+
             <tbody className="bg-white divide-y divide-gray-100">
               {!isLoading &&
-                !isError &&
                 borrowedBooks?.map((book: IProps, index: number) => (
                   <tr key={index} className="hover:bg-gray-50 transition">
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900 font-medium">
